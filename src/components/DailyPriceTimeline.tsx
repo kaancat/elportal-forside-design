2957491564b567b3d4cd05c090d49e963b505d44
kaancat <kc@mondaybrew.dk>
2025-08-02 21:03:46 +0200

    Minimize bottom margin to 5px for maximum chart space
    - Reduce bottom margin from 40px to 5px to match other sides
    - Chart now has minimal 5px margins on top/bottom, 0px on sides
    - Maximizes vertical chart space on mobile
    - X-axis labels still visible with allocated height

--- src/components/DailyPriceTimeline.tsx
+++ src/components/DailyPriceTimeline.tsx
@@ -297,7 +297,7 @@ const DailyPriceTimeline: React.FC<DailyPriceTimelineProps> = ({ block }) => {
                       top: 5, 
                       right: 0, 
                       left: 0, 
-                      bottom: 40 
+                      bottom: 5 
                     }}>
                       <defs>
                         <linearGradient id="colorPriceMobile" x1="0" y1="0" x2="0" y2="1">
